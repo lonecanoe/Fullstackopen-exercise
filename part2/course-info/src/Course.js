fe6a6ca8b456beadb61cd52bf900fd7c4553a541
lonecanoe <69197524+lonecanoe@users.noreply.github.com>
2025-06-06 11:03:05 +0800

    course info

---
+++ part2/course-info/src/Course.js
@@ -0,0 +1,30 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => 
+  <>
+    {parts.map(part => 
+      <Part key={part.id} part={part} />
+    )}
+  </>
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+  return <p><strong>total of {total} exercises</strong></p>
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
